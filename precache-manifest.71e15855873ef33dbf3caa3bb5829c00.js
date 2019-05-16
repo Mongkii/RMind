3e4825406512a9e4df15f3603d9c71dba35f22e4
Jano <mongkii@hotmail.com>
2019-05-16 11:54:59 +0800

    Updates

---
+++ precache-manifest.71e15855873ef33dbf3caa3bb5829c00.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb351bb60ffde443a39e76094246c83e",
+    "url": "/RMind-Demo/index.html"
+  },
+  {
+    "revision": "b960574d4eb982614184",
+    "url": "/RMind-Demo/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "bbe4b6ed6641fc3fdb68",
+    "url": "/RMind-Demo/static/css/main.b3afdc20.chunk.css"
+  },
+  {
+    "revision": "b960574d4eb982614184",
+    "url": "/RMind-Demo/static/js/2.f29f6a22.chunk.js"
+  },
+  {
+    "revision": "cdc5bca6d07c31439615",
+    "url": "/RMind-Demo/static/js/3.482d5276.chunk.js"
+  },
+  {
+    "revision": "596ce449065529b4cdd7",
+    "url": "/RMind-Demo/static/js/4.d151e797.chunk.js"
+  },
+  {
+    "revision": "bbe4b6ed6641fc3fdb68",
+    "url": "/RMind-Demo/static/js/main.15db60e5.chunk.js"
+  },
+  {
+    "revision": "19c4e418a2305a361b8c",
+    "url": "/RMind-Demo/static/js/runtime~main.96029908.js"
+  },
+  {
+    "revision": "0a267abaf4c9d30b8fd7c77cc19625f3",
+    "url": "/RMind-Demo/static/media/zwicon.0a267aba.svg"
+  },
+  {
+    "revision": "64bead59d217d53c5331dcb3d7687814",
+    "url": "/RMind-Demo/static/media/zwicon.64bead59.eot"
+  },
+  {
+    "revision": "cbeae0335fc8290a7611dd00a840391f",
+    "url": "/RMind-Demo/static/media/zwicon.cbeae033.woff"
+  },
+  {
+    "revision": "d7e439cec483969db045235b2bd3a748",
+    "url": "/RMind-Demo/static/media/zwicon.d7e439ce.ttf"
+  }
+]);
